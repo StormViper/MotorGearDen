832f88b224b0279716f684d39392bcdf192298ee
aparker611 <adam3692@hotmail.co.uk>
2016-03-04 17:19:51 +0000

    Fixing add_item_to_cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,31 +1,24 @@
 require 'json'
 class CartController < ApplicationController
 	def add_item_to_cart
-		debugger
+		redirect_to root_path if !user_signed_in?
 		@product = Product.where(:product_id => params[:format]).first
-		@cart = Cart.find(current_user.id)
-		if @cart.cart_slot_one.nil?
-			@cart.cart_slot_one = @product.product_id
-			@cart.save
-			if @cart.save
-				p 'SAVED'
-				@cart.cart_count = @cart.cart_count += 1
-				@cart.save
-			else
-				p 'FAILED'
-			end
-		end
-		if @cart.cart_slot_two.nil?
-			@cart.cart_slot_two = @product.product_id
-			@cart.save
-			if @cart.save
-				p 'SAVED'
-				@cart.cart_count = @cart.cart_count += 1
-				@cart.save
-			else
-				p 'FAILED'
-			end
-		end
+		@cart = Cart.where(:user_id => current_user.id).first
+		@cart_items = [@cart.cart_slot_one, @cart.cart_slot_two, @cart.cart_slot_three, @cart.cart_slot_four, @cart.cart_slot_five,
+									 @cart.cart_slot_six, @cart.cart_slot_seven, @cart.cart_slot_eight, @cart.cart_slot_nine, @cart.cart_slot_ten]
+    @cart_items.each do |item|
+    	if item.nil?
+    		@selected_one = @product.product_id
+    		if !item.nil?
+    			nil
+    		end
+    	end
+    end
+    debugger
+    @cast = Cart.where("user_id = ? AND cart_slot_one = ? AND cart_slot_two = ? AND cart_slot_three = ? AND cart_slot_four = ? AND cart_slot_five = ?
+    										cart_slot_six = ? AND cart_slot_seven = ? AND cart_slot_eight = ? AND cart_slot_nine = ? AND cart_slot_ten = ?", current_user.id, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, ).first
+
+    p 'Hello world'
 		redirect_to root_path
 	end
 
