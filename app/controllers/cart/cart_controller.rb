f85f4ed837b6cb60cd0685077c5952ade0565068
aparker611 <adam3692@hotmail.co.uk>
2016-03-14 17:08:04 +0000

    Added cart view, updating homepage so cart is not there and linked our cart header to our cart

--- app/controllers/cart/cart_controller.rb
+++ app/controllers/cart/cart_controller.rb
@@ -71,4 +71,27 @@ class Cart::CartController < ApplicationController
 		@cart.destroy
 		redirect_to root_path
 	end
+
+	def show
+		@cart = Cart.where(:user_id => current_user.id).first if user_signed_in?
+		@slots = @cart.slots.first
+		@slot_list = [@slots.slot_one, @slots.slot_two, @slots.slot_three, @slots.slot_four, @slots.slot_five,
+								  @slots.slot_six, @slots.slot_seven, @slots.slot_eight, @slots.slot_nine, @slots.slot_ten]
+    @user_products = []
+    @product = []
+		@slot_list.each do |item|
+		if item.nil?
+				p 'Item empty'
+			else				
+				@product << item
+		end
+		end
+			@product.each do |item|
+			items = Product.where(:product_id => item).first
+			@user_products << items
+		end
+
+		#@total = @user_products[0].product_price + @user_products[1].product_price + @user_products[2].product_price + @user_products[3].product_price + @user_products[4].product_price + @user_products[5].product_price
+					 										 #+ @user_products[6].product_price + @user_products[7].product_price + @user_products[8].product_price + @user_products[9].product_price
+	end
 end
