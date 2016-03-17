82ea7a6735d18c4bf8adf3a4323fc5e38f3176ea
aparker611 <adam3692@hotmail.co.uk>
2016-03-17 15:07:03 +0000

    Built relationship between everything, created brand with relationship to product and user

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :products
-	has_many :slots, dependent: :destroy
+	has_many :slot, dependent: :destroy
+	belongs_to :user
 	validates :user_id, presence: true, uniqueness: true
 end
