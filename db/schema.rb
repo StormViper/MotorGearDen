d92fd0ec29864a83ace6798be4d1c12e65b06e0c
aparker611 <adam3692@hotmail.co.uk>
2016-03-29 15:12:20 +0100

    Updated registration email and _details partial and rake task to add address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329130428) do
+ActiveRecord::Schema.define(version: 20160329135750) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 20160329130428) do
     t.datetime "updated_at",         null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
