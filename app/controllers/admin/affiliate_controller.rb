b3f11eaa30877c5bf2b637c083430a76cd743206
aparker611 <adam3692@hotmail.co.uk>
2016-03-31 14:36:24 +0100

    Auto stash for cherrypick of "Updated how % works on brands"
    Added same menu for Affiliates using the template from brand - used AJAX.

--- app/controllers/admin/affiliate_controller.rb
+++ app/controllers/admin/affiliate_controller.rb
@@ -5,6 +5,34 @@ class Admin::AffiliateController < ApplicationController
 		@affiliate = Affiliate.new
 	end
 
+	def menu
+		@affiliates = Affiliate.search(params[:search])
+		@affiliate = Affiliate.new
+	end
+
+	def edit
+		@affiliate = Affiliate.find(params[:format])
+	end
+
+	def update
+		@affiliate = Affiliate.where(name: params[:affiliate][:name]).first
+		@is_enabled = params[:affiliate][:is_enabled]
+		if @is_enabled = 0 && @affiliate.is_enabled?
+			@affiliate.toggle! :is_enabled?
+		else
+			if @is_enabled = 1 && !@affiliate.is_enabled?
+				@affiliate.toggle! :is_enabled?
+			end
+		end
+		if @affiliate.update(affiliate_params)
+			flash[:success] = "Saved affiliate settings"
+			redirect_to affiliate_menu_path
+		else
+			flash[:danger] = "Error occured please check settings"
+			redirect_to affiliate_menu_path
+		end
+	end
+
 	def create
 		@affiliate = Affiliate.new(affiliate_params)
 		@is_enabled = params[:is_enabled?]
@@ -13,7 +41,7 @@ class Admin::AffiliateController < ApplicationController
 		end
 		if @affiliate.save
 			flash[:success] = "Affiliate created"
-			redirect_to root_path
+			redirect_to affiliate_menu_path
 		else
 			flash[:danger] = 'Error: Please try again'
 			render 'new'
