fb45de806432a41f939fd0326947c6c2d34d216d
aparker611 <adam3692@hotmail.co.uk>
2016-04-01 09:22:39 +0100

    Revert "Develop"

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,15 +2,15 @@ class ChargesController < ApplicationController
 	before_action :authenticate_user!
 	def new
 		@cart = Cart.where(:user_id => current_user.id).first if user_signed_in?
-		@user_products = UserManager.get_user_products!(current_user.id)
-		@total = UserManager.get_user_total!(current_user)
+		@user_products = get_user_products!
+		@total = get_user_total!
 
 		@cart.total = @total
 		@cart.save!
 	end
 
 	def create
-		@user_products = UserManager.get_user_products!(current_user.id)
+		@user_products = get_user_products!
 		@cart = current_user.cart
 		@total = @cart.total
 		@amount = @cart.total * 100
@@ -29,7 +29,7 @@ class ChargesController < ApplicationController
 
 		Email.checkout_email(current_user, @user_products).deliver
 
-		@brands = BrandManager.get_brands!(current_user, @user_products)
+		@brands = get_brands!(current_user, @user_products)
 		@brands.each do |brand|
 			Email.checkout_notify_brand_email(brand, @user_products).deliver_now
 		end
